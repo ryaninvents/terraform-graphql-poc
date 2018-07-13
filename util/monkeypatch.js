cec1a9b0570cb4b55b600c3733688e41fc0d3632
Ryan Kennedy <ryaninvents@users.noreply.github.com>
2018-07-13 19:58:06 -0400

    feat: implement server code bundling

--- util/monkeypatch.js
+++ util/monkeypatch.js
@@ -1,4 +1,6 @@
-require('stream').Readable.prototype.then = function then (...args) {
+const {Readable} = require('stream')
+
+Readable.prototype.then = function then (...args) {
   return new Promise((resolve, reject) => {
     const bufs = []
     this.on('error', reject)
@@ -7,3 +9,8 @@ require('stream').Readable.prototype.then = function then (...args) {
   })
     .then(...args)
 }
+
+exports.promisify = (stream) => new Promise((resolve, reject) => {
+  stream.on('end', resolve)
+  stream.on('error', reject)
+})
