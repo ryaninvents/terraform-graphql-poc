6bf06fe5f2813b1e8045a9c3eb4d92a2fc4aa826
Ryan Kennedy <ryaninvents@users.noreply.github.com>
2018-07-25 19:46:59 -0400

    feat: frontend "login" link logs in

--- packages/web/index.js
+++ packages/web/index.js
@@ -1,12 +1,21 @@
-import './style';
-import {Component} from 'preact';
+import './style'
+import 'preact-compat'
+import {Component} from 'preact'
+import {Helmet} from 'react-helmet'
+
+import config from './config'
 
 export default class App extends Component {
-  render() {
+  render () {
     return (
-      <div>
-        <h1>Hello, World!</h1>
+      <div class="container">
+        <Helmet>
+          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.1.2/litera/bootstrap.min.css" />
+          <title>terraform-graphql-poc</title>
+        </Helmet>
+        <h1>terraform-graphql-poc</h1>
+        <a class="btn btn-success text-light" href={`${config.apiEndpoint}/login`}>Log in</a>
       </div>
-    );
+    )
   }
 }
